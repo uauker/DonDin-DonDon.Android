aa33a71b6d3ad645ac37698e44b6f443bffa3a14
uauker <uauker@gmail.com>
2016-01-28 16:43:08 -0200

    fazendo o botao don din funcionar

--- app/src/main/java/com/uauker/apps/dondindondon/ScrollingActivity.java
+++ app/src/main/java/com/uauker/apps/dondindondon/ScrollingActivity.java
@@ -1,5 +1,7 @@
 package com.uauker.apps.dondindondon;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +10,20 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class ScrollingActivity extends AppCompatActivity {
+
+    Button btnDonDin;
+    Button btnDonDon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +41,8 @@ public class ScrollingActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        this.setButtons();
     }
 
     @Override
@@ -48,5 +64,62 @@ public class ScrollingActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void setButtons() {
+        this.btnDonDin = (Button) findViewById(R.id.don_din);
+        this.btnDonDon = (Button) findViewById(R.id.don_don);
+
+        this.btnDonDin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    final Uri uri = Uri.parse(String.format("android-resource://%s/%s", getPackageName(), R.raw.dondin));
+
+
+                    FileInputStream file = playAudio("dondin.m4a");
+
+                    final MediaPlayer mediaPlayer = new MediaPlayer();
+                    mediaPlayer.setDataSource(file.getFD());
+                    mediaPlayer.prepare();
+                    mediaPlayer.start();
+                } catch (IOException e) {
+                    //TODO: Tratar o erro
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private FileInputStream playAudio(final String mediaUrl) {
+        try {
+            final InputStream is = getResources().openRawResource(R.raw.dondin);
+
+            // create file to store audio
+            final File mediaFile = new File(this.getCacheDir(), "tmp");
+            final FileOutputStream fos = new FileOutputStream(mediaFile);
+            final byte buf[] = new byte[16 * 1024];
+
+            // write to file until complete
+            do {
+                final int numread = is.read(buf);
+
+                if (numread <= 0) {
+                    break;
+                }
+
+                fos.write(buf, 0, numread);
+            } while (true);
+
+            fos.flush();
+            fos.close();
+
+            return new FileInputStream(mediaFile);
+        } catch (Exception e) {
+            //TODO:tratar erro
+            e.printStackTrace();
+        }
+        //TODO:null eh feio... exception por favor
+        return null;
     }
 }
