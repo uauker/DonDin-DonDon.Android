d46e323e400189ada9a58a51a60e19480c9a1de4
uauker <uauker@gmail.com>
2016-01-28 17:36:05 -0200

    excluindo código desnecessário no momento

--- app/src/main/java/com/uauker/apps/dondindondon/activities/ScrollingActivity.java
+++ app/src/main/java/com/uauker/apps/dondindondon/activities/ScrollingActivity.java
@@ -40,27 +40,6 @@ public final class ScrollingActivity extends AppCompatActivity {
         this.setButtons();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_scrolling, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
     private void setButtons() {
         this.btnDonDin = (Button) findViewById(R.id.don_din);
         this.btnDonDin.setOnClickListener(new View.OnClickListener() {
