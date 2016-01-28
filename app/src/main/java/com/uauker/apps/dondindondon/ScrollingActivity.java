8b9123984992d4908524621bb1c02c7e60a7af24
uauker <uauker@gmail.com>
2016-01-28 17:01:37 -0200

    incluindo o timber para log

--- app/src/main/java/com/uauker/apps/dondindondon/ScrollingActivity.java
+++ app/src/main/java/com/uauker/apps/dondindondon/ScrollingActivity.java
@@ -17,6 +17,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
+import timber.log.Timber;
+
 public class ScrollingActivity extends AppCompatActivity {
 
     Button btnDonDin;
